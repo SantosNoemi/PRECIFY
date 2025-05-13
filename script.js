4e14f03b223c345dba9a5b5beff73fe15a4e54fd
Noemi Santos de Oliveira <osantosnoemi@gmail.com>
2025-05-13 19:56:41 -0300

    pastas

---
+++ script.js
@@ -0,0 +1,102 @@
+{
+  "resumo"= [
+      {
+          "id": 1,
+        "janela": "Resumo",
+        "titulo": "Relatórios Gerenciais - Resumo",
+        "relatorio": "Planilha",
+        "grafico1": "Progresso",
+        "grafico2": "Estatísticas",
+        "botaoGerar": "Gerar relatório",
+        "botaoAdicionar": "Adicioar",
+        "botaoVisualizar": "Visualizar",
+        "botaoDashboard": "Dashboard"
+      }
+  ],
+
+  "Relatorio Planilha"= [
+      {
+      "id": 2,
+      "janela": "Relatorio Planilha",
+      "titulo": "Relatórios Gerenciais",
+      "relatorio": "Planilha",
+      "botaoGerar": "Gerar relatório",
+      "botaoAdicionar": "Adicioar",
+      "botaoVisualizar": "Visualizar",
+      "botaoDashboard": "Dashboard",
+      "planilha": [
+          {
+              "id": 3,
+              "titulo": "Relatório de Coletas",
+              "descrição": "Relatório mensal das coletas realizadas.",
+              "arquivo": "coleta_jan2025.pdf",
+              "data": "01-02-2025"
+          },
+          {
+              "id": 4,
+              "titulo": "Materiais Reciclados",
+              "descrição": "Quantidade de materiais reciclados por tipo.",
+              "arquivo": "reciclados_fev2025.pdf",
+              "data": "03-03-2025"
+          },
+          {
+              "id": 5,
+              "titulo": "Empresas Atendidas",
+              "descrição": "Lista de empresas atendidas no trimestre.",
+              "arquivo": "empresas_mar2025.pdf",
+              "data": "05-04-2025"
+          },
+          {
+              "id": 6,
+              "titulo": "Catadores Ativos",
+              "descrição": "Número de catadores ativos por região.",
+              "arquivo": "catadores_abr2025.pdf",
+              "data": "06-05-2025"
+          },
+          {
+              "id": 7,
+              "titulo": "Análise Financeira",
+              "descrição": "Análise de custos e lucros das operações.",
+              "arquivo": "financeiro_mai2025.pdf",
+              "data": "07-06-2025"
+          },
+          {
+              "id": 8,
+              "titulo": "Impacto Ambiental",
+              "descrição": "Relatório de impacto ambiental evitado.",
+              "arquivo": "impacto_jun2025.pdf",
+              "data": "08-07-2025"
+          }]
+      }
+  ],
+
+  "grafico"=[
+      {
+          "id": 9,
+          "janela": "Gráficos",
+          "titulo": "Gráficos",
+          "relatorio": "Grafico",
+          "grafico1": "Coletas Gerais",
+          "grafico2": "1º Semestre",
+          "grafico3": "Progresso",
+          "botaoGerar": "Gerar relatório",
+          "botaoVisualizar": "Visualizar"
+      }
+  ],
+
+  "estatisticas"=[
+      {
+          "id": 10,
+          "janela": "estatisticas",
+          "titulo": "Estatisticas",
+          "relatorio": "Grafico",
+          "grafico1": "1º Semestre",
+          "grafico2": "2º Semestre",
+          "card1": "img",
+          "card2": "img",
+          "botaoGerar": "Gerar relatório",
+          "botaoVisualizar": "Visualizar"
+      }
+  ]    
+}
+
